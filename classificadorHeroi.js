32a5dfdd08e7fbfc5abbefa9f8ee37cccc26f8b7
iasminsp <130411997+iasminsp@users.noreply.github.com>
2025-08-07 08:26:46 -0300

    desafio de classificaçao de heroi, DIO

---
+++ classificadorHeroi.js
@@ -0,0 +1,29 @@
+let herois =[
+    {nome: "Iasmin", xp: 7450},
+    {nome:"felipe", xp: 15000}
+]
+
+for (let i = 0; i<herois.length; i++){
+    let xp = herois[i].xp;
+    let nivel = "";
+    
+    if (xp < 1000) {
+        nivel = "Ferro";
+    } else if (xp >= 1000 && xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp >= 2001 && xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp >= 5001 && xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp >= 7001 && xp <= 8000) {
+        nivel = "Platina";
+    } else if (xp >= 8001 && xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp >= 9001 && xp <= 10000) {
+        nivel = "Imortal";
+    } else if (xp >= 10001) {
+        nivel = "Radiante";
+    }
+
+    console.log(`O Herói de nome **${herois[i].nome}** está no nível de **${nivel}**`);
+}
